65f515bec1785bf16002bccfb04908706a876e36
Kay <hiebertkay@gmail.com>
2024-01-02 20:39:50 -0500

    added delete function

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,11 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
+const DB_LOCATION = './db/notes.json';
+
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile(DB_LOCATION).then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new note
@@ -20,21 +22,25 @@ notes.post('/', (req, res) => {
       id: uuidv4(),
     };
 
-    readAndAppend(newNote, './db/notes.json');
+    readAndAppend(newNote, DB_LOCATION);
     res.json(newNote);
   } else {
     res.error('Error in adding note');
   }
 });
 
-// POST Route for a new note
+// DELETE Route for a note
 notes.delete('/:id', (req, res) => {
-  console.log(req.body);
-
   const id = req.params.id;
   if (id) {
-
-    res.json('');
+    readFromFile(DB_LOCATION)
+      .then((data) => {
+        const notes = JSON.parse(data);
+        const filteredNotes = notes.filter(note => note.id !== id);
+        writeToFile(DB_LOCATION, filteredNotes);
+        res.json('');
+      })
+      .catch(err => res.error('Error in deleting note:', err));
   } else {
     res.error('Error in deleting note');
   }
